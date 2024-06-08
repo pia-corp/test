f369f3d2963a9d2120b164ed245c02c71c0c2fd8
pia <sysadmin@pia-corp.jp>
2024-06-09 01:39:49 +0900

    config修正

--- next.config.mjs
+++ next.config.mjs
@@ -1,57 +1,28 @@
+import { fileURLToPath } from 'url';
+import path from 'path';
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   output: 'export',
+  experimental: {
+    swcMinify: true,
+    cpus: 4,
+  },
   generateBuildId: async () => {
     return 'pia';
   },
-  // キャッシュの有効化
   webpack: (config, { isServer }) => {
     if (!isServer) {
       config.cache = {
         type: 'filesystem',
         allowCollectingMemory: true,
         buildDependencies: {
-          config: [__filename],
+          config: [fileURLToPath(import.meta.url)],
         },
       };
     }
     return config;
-  },
-  // Image Optimization
-  images: {
-    domains: ['your-image-source.com'], // ドメイン名は適宜変更してください
-    deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
-    imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
-  },
-  // 静的ファイルのキャッシュ設定
-  async headers() {
-    return [
-      {
-        source: '/(.*)',
-        headers: [
-          {
-            key: 'Cache-Control',
-            value: 'public, max-age=31536000, immutable',
-          },
-        ],
-      },
-    ];
-  },
-  // CSS Minification
-  cssLoaderOptions: {
-    importLoaders: 1,
-    modules: {
-      localIdentName: '[hash:base64:5]',
-    },
-  },
-  // ビルド並列化
-  experimental: {
-    swcLoader: true,
-    swcMinify: true,
-    cpus: 4,
-    concurrentFeatures: true,
-    serverComponents: true,
   },
 };
 
